6be35a65be0c925e1d1c55e785336032569e9ae6
thien_tran <thien1892.tech@gmail.com>
2023-02-17 12:44:13 +0700

    add tao ho so

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.title("Tool hỗ trợ công việc")
+st.header("Danh sách các app đã làm:")
+st.write("1. Tạo hồ sơ theo file")
+
+st.header("Danh sách các app đang làm:")
+st.write("1. Tạo hồ sơ đơn lẻ")
+st.write("2. Tạo hồ sơ từ nhiều CMT")
+st.write("3. Tạo hồ sơ từ 1 CMT")
+st.write("4. Chuyển ảnh thành PDF")
+st.write("5. Nén file PDF")
---
+++ app/pages/Ho so theo file.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+from docxtpl import DocxTemplate
+from docxcompose.composer import Composer
+from docx import Document
+import os
+# import base64
+# import uuid
+# import re
+
+PATH_FILE_MAU = "merge_mail/IMPOT_CHAN_CHAN_fix.xls"
+PATH_FILE_WORD = "merge_mail/HO_SO_PL_PY.docx"
+
+doc = DocxTemplate(PATH_FILE_WORD)
+
+with st.sidebar:
+    st.write("Download file mẫu:")
+    with open(PATH_FILE_MAU, 'rb') as my_file:
+        st.download_button(label = 'file_mau_ho_so.xls',
+                        data = my_file,
+                        file_name = 'file_mau_ho_so.xls')
+
+st.header('Làm hồ sơ theo file')
+st.write('''
+    Click vào **Browes files** để up file excel. Bấm **UPLOAD!** để xác nhận.
+    ''')
+
+with st.form("Tải file cập nhật", clear_on_submit=True):
+    file_name = st.file_uploader('',type = ['.xls'])
+    submitted = st.form_submit_button("UPLOAD!")
+
+
+# file = st.file_uploader("Upload file excel", key="file_uploader", type = 'xls')
+if submitted and file_name is not None:
+    df = pd.read_excel(file_name, dtype = {'SĐT Đăng ký': str, 'Số GTTT': str})
+    df = df.dropna(subset=['SĐT Đăng ký', 'Họ tên'])
+    df = df.fillna(" ")
+
+    name_col = df.columns.to_list()
+    name_col = ["_".join(i.split(' ')) for i in name_col]
+    text_col = [" "] * len(name_col)
+
+    df.columns = name_col
+
+    for index, row in df.iterrows():
+        row_ct = [str(row[i]) for i in name_col]
+        context = dict(zip(name_col, row_ct))
+        doc.render(context)
+        if index == 0:
+            doc.save(f"merge_mail/ho_so_{row['SĐT_Đăng_ký']}.docx")
+            master = Document(f"merge_mail/ho_so_{row['SĐT_Đăng_ký']}.docx")
+            composer = Composer(master)
+            os.remove(f"merge_mail/ho_so_{row['SĐT_Đăng_ký']}.docx")
+        else:
+            composer.append(doc)
+        # doc.save(f"merge_mail/ho_so_{row['SĐT_Đăng_ký']}.docx")
+    str_time = datetime.now().strftime("%H-%M_%d-%m-%Y")
+
+    name_file_ho_so = f"merge_mail/hoso-{row['User_phát_triển_TB']}-{str_time}.docx"
+    name_file_tai_ve = f"hoso-{row['User_phát_triển_TB']}-{str_time}.docx"
+    composer.save(name_file_ho_so)
+
+
+    st.write("File hồ sơ đã tạo xong! Bấm để tải file về:")
+    with open(name_file_ho_so, 'rb') as my_file:
+        st.download_button(label = name_file_tai_ve,
+                            data = my_file,
+                            file_name = name_file_tai_ve)
