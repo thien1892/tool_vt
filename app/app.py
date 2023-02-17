79ed40c65f165ada00ab98eb338517815b9c0ced
thien_tran <thien1892.tech@gmail.com>
2023-02-17 22:03:02 +0700

    fix some error

--- app/app.py
+++ app/app.py
@@ -3,20 +3,24 @@ import glob
 import datetime
 import os
 
-def check_time_file(file_):
-    return (datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getmtime(file_))).total_seconds() / 3600.0
 
 
 st.title("Tool hỗ trợ công việc")
 st.header("Danh sách các app đã làm:")
 st.write("1. Tạo hồ sơ theo file")
+st.write("2. Tạo hồ sơ đơn lẻ")
 
 st.header("Danh sách các app đang làm:")
-st.write("1. Tạo hồ sơ đơn lẻ")
-st.write("2. Tạo hồ sơ từ nhiều CMT")
-st.write("3. Tạo hồ sơ từ 1 CMT")
-st.write("4. Chuyển ảnh thành PDF")
-st.write("5. Nén file PDF")
+
+st.write("1. Tạo hồ sơ từ nhiều CMT")
+st.write("2. Tạo hồ sơ từ 1 CMT")
+st.write("3. Chuyển ảnh thành PDF")
+st.write("4. Nén file PDF")
+
+
+
+def check_time_file(file_):
+    return (datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getmtime(file_))).total_seconds() / 3600.0
 
 
 FILE_GOC = ['merge_mail/IMPOT_CHAN_CHAN_fix.xls', 'merge_mail/HO_SO_PL_PY.docx']
